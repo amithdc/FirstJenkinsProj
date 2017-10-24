f6fc96e18bfaa3153e2dbed0d5c22a76b705707a
Training_H2A.06.18 <Training_h2a.06.18@accenture.com>
2017-10-24 12:00:14 +0530

    Initial commit

---
+++ src/main/java/Development_Oct/Develop/App.java
@@ -0,0 +1,13 @@
+package Development_Oct.Develop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
